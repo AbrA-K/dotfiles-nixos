11db993452b3b7c9ef07e587f088c7f18a8bffe3
abra-k <matthias.vo2107@gmail.com>
2024-04-12 00:20:06 +0200

    forgor the flake

--- flake.nix
+++ flake.nix
@@ -6,9 +6,11 @@
 
   inputs = {
     nixpkgs.url = "github:NixOs/nixpkgs/nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, ... }:
+  outputs = { self, home-manager, nixpkgs, ... }:
   let
     lib = nixpkgs.lib;
   in {
@@ -16,8 +18,31 @@
       KadaZen = lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
-          /etc/nixos/hardware-configuration.nix
+          ./hypr/hyprland.nix
+          ./nushell/nu.nix
+          ./hardware-configuration.nix
           ./old-config.nix
+          ./syncthing.nix
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.abra = import ./home.nix;
+          }
+        ];
+      };
+      KadaZen-gnome = lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./gnome-config.nix
+          ./hardware-configuration.nix
+          ./old-config.nix
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.abra = import ./home.nix;
+          }
         ];
       };
     };
